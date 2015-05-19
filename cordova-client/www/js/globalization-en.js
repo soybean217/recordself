9a4950337fd44aba76fa87f99536237df8d663a8
soybean217 <13565644@qq.com>
2015-05-19 09:49:22 -0700

    keep backup

--- cordova-client/www/js/globalization-en.js
+++ cordova-client/www/js/globalization-en.js
@@ -1,4 +1,6 @@
 var globalization_password = "Password";
 var globalization_confirm_password = "Confirm Password";
-var globalization_register = "Register";
+var globalization_sign_up = "Sign Up";
+var globalization_sign_in_exist = "Sign In With Exist Account";
 var globalization_username = "Username";
+var globalization_begin = "Begin";
--- cordova-client/www/js/globalization-zh.js
+++ cordova-client/www/js/globalization-zh.js
@@ -1,4 +1,6 @@
 var globalization_password = "密码";
 var globalization_confirm_password = "确认密码";
-var globalization_register = "注册";
+var globalization_sign_up = "注册";
+var globalization_sign_in_exist = "使用已有账户登录";
 var globalization_username = "用户名";
+var globalization_begin = "开始";
