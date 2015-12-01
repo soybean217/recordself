d414f45b2eca53bd651aee7fbdb1646d43cefa4b
soybean217 <13565644@qq.com>
2015-12-01 09:01:42 +0800

    continue hex add .

--- cordova-client/www/js/common-hex-add.js
+++ cordova-client/www/js/common-hex-add.js
@@ -2,20 +2,31 @@ var HEX_MAP = ['2','3','4','5','6','7','8','9','A','B','C','D','E','F','G','H','
 var LENGTH_FIX = 13 ;
 
 function hexAddOne(input){
-	var output;
+	var input;
 	var goOn = true ;
 	var index = LENGTH_FIX-1;
 	while (goOn&&index>=0){
-		if (input[index]!=HEX_MAP[HEX_MAP.length-1]){
-			
-		}else{
-			
+		// get index of HEX_MAP
+		var mapIndex = -1 ;
+		for (var i = 0 ;i<HEX_MAP.length;i++){
+			if (input[index]==HEX_MAP[i]){
+				mapIndex = i;
+				if (input[index]!=HEX_MAP[HEX_MAP.length-1]){
+					input[index]=
+				}else{
+					
+				}
+			}
+		}
+		if (mapIndex==-1){
+			console.log("illegal input");
+			break;
 		}
 		index--;
 	}
 	if (i==0){
 		console.log("overload");
-		output = "0";
+		input = "0";
 	}
-	return output;
+	return input;
 }
