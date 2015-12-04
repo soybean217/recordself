6da0c9bac7d7bafa324881ce2a2d571b00122676
soybean217 <13565644@qq.com>
2015-12-04 09:04:06 +0800

    need change to string

--- cordova-client/www/js/common-hex-add.js
+++ cordova-client/www/js/common-hex-add.js
@@ -5,28 +5,44 @@ function hexAddOne(input){
 	var input;
 	var goOn = true ;
 	var index = LENGTH_FIX-1;
-	while (goOn&&index>=0){
-		// get index of HEX_MAP
-		var mapIndex = -1 ;
-		for (var i = 0 ;i<HEX_MAP.length;i++){
-			if (input[index]==HEX_MAP[i]){
-				mapIndex = i;
-				if (input[index]!=HEX_MAP[HEX_MAP.length-1]){
-					input[index]=
-				}else{
-					
+	var output = [];
+	for (index;index>=0;index--){
+		if (goOn){
+			// get index of HEX_MAP
+			var mapIndex = -1 ;
+			for (var i = 0 ;i<HEX_MAP.length;i++){
+				console.log(input[index]+":"+HEX_MAP[i]);
+				if (input[index]==HEX_MAP[i]){
+					console.log(input[index]+":"+HEX_MAP[i]);
+					mapIndex = i;
+					if (input[index]!=HEX_MAP[HEX_MAP.length-1]){
+						output[index]=HEX_MAP[i+1];
+						console.log("true:"+input[index]+":"+i+":"+HEX_MAP[i+1]);
+						goOn = false;
+					}else{
+						output[index]=HEX_MAP[0];
+						console.log("else:"+input[index]);
+					}
+					break;
 				}
 			}
+			if (mapIndex==-1){
+				output = [];
+				console.log("illegal input");
+				break;
+			}
+		}else{
+			output[index]=input[index];
 		}
-		if (mapIndex==-1){
-			console.log("illegal input");
-			break;
-		}
-		index--;
+		console.log(output);
 	}
-	if (i==0){
+	if (index<-1){
 		console.log("overload");
-		input = "0";
+		input = [];
 	}
-	return input;
+	var strOutput = new String() ;
+	for (var i = 0 ; i<output.length;i++){
+		strOutput.concat(output[i]);
+	}
+	return strOutput;
 }
