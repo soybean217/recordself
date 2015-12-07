1c0170d1b82973aa7f2579ee9167a16e3ed68b22
soybean217 <13565644@qq.com>
2015-12-07 08:43:00 +0800

    add test

--- cordova-client/www/js/common-hex-add.js
+++ cordova-client/www/js/common-hex-add.js
@@ -6,43 +6,58 @@ function hexAddOne(input){
 	var goOn = true ;
 	var index = LENGTH_FIX-1;
 	var output = [];
-	for (index;index>=0;index--){
-		if (goOn){
-			// get index of HEX_MAP
-			var mapIndex = -1 ;
-			for (var i = 0 ;i<HEX_MAP.length;i++){
-				console.log(input[index]+":"+HEX_MAP[i]);
-				if (input[index]==HEX_MAP[i]){
-					console.log(input[index]+":"+HEX_MAP[i]);
-					mapIndex = i;
-					if (input[index]!=HEX_MAP[HEX_MAP.length-1]){
-						output[index]=HEX_MAP[i+1];
-						console.log("true:"+input[index]+":"+i+":"+HEX_MAP[i+1]);
-						goOn = false;
-					}else{
-						output[index]=HEX_MAP[0];
-						console.log("else:"+input[index]);
+	var strOutput = new String() ;
+	if (input.length==LENGTH_FIX){
+		for (index;index>=0;index--){
+			if (goOn){
+				// get index of HEX_MAP
+				var mapIndex = -1 ;
+				for (var i = 0 ;i<HEX_MAP.length;i++){
+					if (input[index]==HEX_MAP[i]){
+						mapIndex = i;
+						if (input[index]!=HEX_MAP[HEX_MAP.length-1]){
+							output[index]=HEX_MAP[i+1];
+							goOn = false;
+						}else{
+							output[index]=HEX_MAP[0];
+						}
+						break;
 					}
+				}
+				//overload
+				if (goOn&&index==0&&output[index]==HEX_MAP[0]){
+					console.log("error input : "+input+" overload");
+					output = [];
+				}
+				if (mapIndex==-1){
+					output = [];
+					console.log("error illegal input:"+input+" . not in map");
 					break;
 				}
+			}else{
+				output[index]=input[index];
 			}
-			if (mapIndex==-1){
-				output = [];
-				console.log("illegal input");
-				break;
-			}
-		}else{
-			output[index]=input[index];
 		}
-		console.log(output);
-	}
-	if (index<-1){
-		console.log("overload");
-		input = [];
-	}
-	var strOutput = new String() ;
-	for (var i = 0 ; i<output.length;i++){
-		strOutput.concat(output[i]);
+		for (var i in output){
+			strOutput += output[i];
+		}
+	}else{
+		output = [];
+		console.log("error illegal input:"+input+" , length must equals :"+LENGTH_FIX);
 	}
 	return strOutput;
 }
+function testHexAddOne(){
+	var  input = "746VHXEAVQY43";
+	console.log(input+";"+hexAddOne(input));
+	var  input = "ZZ";
+	console.log(input+";"+hexAddOne(input));
+	var  input = "3ZZZZZZZZZZZZ";
+	console.log(input+";"+hexAddOne(input));
+	var  input = "ZZZZZZZZZZZZZ";
+	console.log(input+";"+hexAddOne(input));
+	var  input = "ZZZZZZZ0ZZZZZ";
+	console.log(input+";"+hexAddOne(input));
+	var  input = "ZZZZZZZZZZZZZ2";
+	console.log(input+";"+hexAddOne(input));
+} 
