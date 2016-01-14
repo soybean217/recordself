1609ee28d154c4f461691ebc786f6723c5949039
soybean217 <13565644@qq.com>
2016-01-14 08:54:19 +0800

    modify server id server check

--- server/src/main/java/com/recordself/service/RelationService.java
+++ server/src/main/java/com/recordself/service/RelationService.java
@@ -12,6 +12,7 @@ import org.apache.commons.dbutils.handlers.BeanListHandler;
 import org.apache.log4j.Logger;
 import org.common.util.Base;
 import org.common.util.ConnectionService;
+import org.common.util.HexChange;
 
 import com.recordself.entity.Relation;
 import com.recordself.entity.User;
@@ -67,9 +68,9 @@ public class RelationService {
       StringBuilder sql = new StringBuilder();
       // perhaps thread issue , think about lock
       for (Relation record : receiveWithServerIdList) {
-        if (Base.isNumeric(record.getServerId()) && Long.parseLong(record.getServerId()) > 0) {
-          sql.append("union all SELECT " + SQL_COLUMN + " FROM server_relations where serverId = "
-              + record.getServerId() + " ");
+        if (record!=null&&record.getServerId()!=null&&record.getServerId().length()==HexChange.OUTPUT_FIX_LENGTH) {
+          sql.append("union all SELECT " + SQL_COLUMN + " FROM server_relations where serverId = '"
+              + record.getServerId() + "' ");
         } else {
           LOG.error(record.getServerId() + ":not correct . user id :" + user.getUserId());
         }
