0236bf719c01efe0b6cf59c99f69fe1ab3a96e42
soybean217 <13565644@qq.com>
2016-04-15 20:04:45 +0800

    modify server URL to product

--- cordova-client/www/js/record-parameters.js
+++ cordova-client/www/js/record-parameters.js
@@ -1,2 +1,2 @@
-var mServerUrl = "http://FM-X230:8080/recordself-server/for-client/";
-//var mServerUrl = "http://nowiamdoing.com/for-client/";
+//var mServerUrl = "http://FM-X230:8080/recordself-server/for-client/";
+var mServerUrl = "http://nowiamdoing.com/for-client/";
